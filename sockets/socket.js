b39a7c8297013931b3bc924e8365914fe105fae3
moisesfa <moifdez@gmail.com>
2021-01-16 21:14:39 +0100

    Servidor basico

---
+++ sockets/socket.js
@@ -0,0 +1,20 @@
+const { io } = require('../index');
+
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje-ic', ( payload ) => {
+        console.log('Mensaje recibido del cliente', payload);
+
+        io.emit( 'mensaje-is', { server: 'Respuesta del servidor' } );
+
+    });
+
+
+});
